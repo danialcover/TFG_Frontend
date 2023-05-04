62fde10851cf65f1f99f87be3ab482a46655dc79
danielalcover1 <1562889@uab.cat>
2023-05-04 14:10:35 +0200

    Creat component de detall d'una lliga.

---
+++ TFG_Frontend/src/app/presentation/league-detail/league-detail.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {LeagueRepository} from "../../core/league/league.repository";
+import {League} from "../../core/league/league";
+import {Group} from "../../core/league/group/group";
+import {GroupRepository} from "../../core/league/group/group.repository";
+
+@Component({
+  selector: 'app-league-detail',
+  templateUrl: './league-detail.component.html',
+  styleUrls: ['./league-detail.component.scss']
+})
+export class LeagueDetailComponent implements OnInit {
+
+  leagueId: number = 0;
+  league: League = new League();
+  groups: Group[] = [];
+  filterGroup?: Group;
+
+  constructor(private route: ActivatedRoute,
+              private leagueRepo: LeagueRepository,
+              private groupRepo: GroupRepository) {
+  }
+
+  ngOnInit(): void {
+    this.leagueId = Number(this.route.snapshot.paramMap.get('id'));
+    this.leagueRepo.get(this.leagueId).subscribe((league: League) => {
+      this.league = league;
+    });
+    this.groupRepo.getList().subscribe((groups: Group[]) => {
+      this.groups = groups
+        .filter(group => group.league == this.leagueId)
+        .sort((group1, group2) => {
+          if (group1.id === null || group2.id === null) {
+            return 0;
+          } else {
+            return group1.id - group2.id;
+          }
+        });
+      this.filterGroup = this.groups[0];
+    });
+  }
+
+}
