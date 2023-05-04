09738a59038921bc212a6e27fcebae1518c7feec
danielalcover1 <1562889@uab.cat>
2023-05-04 14:05:54 +0200

    Afegit bootstrap al projecte. Canviat app component per tenir la nav bar i el router a sota.

--- TFG_Frontend/src/app/app.module.ts
+++ TFG_Frontend/src/app/app.module.ts
@@ -1,18 +1,60 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LeagueListComponent} from './presentation/league-list/league-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {AppProviders} from "./di/providers";
+import {NavBarComponent} from './presentation/nav-bar/nav-bar.component';
+import {RouterModule, Routes} from '@angular/router';
+import {MainPageComponent} from './presentation/main-page/main-page.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatListModule} from "@angular/material/list";
+import {MatTableModule} from "@angular/material/table";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import { LeagueDetailComponent } from './presentation/league-detail/league-detail.component';
+import {MatTabsModule} from "@angular/material/tabs";
+
+
+const appRoutes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: 'leagues-list', component: LeagueListComponent},
+  { path: 'league-detail/:id', component: LeagueDetailComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeagueListComponent,
+    NavBarComponent,
+    MainPageComponent,
+    LeagueDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatListModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AppProviders],
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
-export class AppModule { }
+export class AppModule {
+}
