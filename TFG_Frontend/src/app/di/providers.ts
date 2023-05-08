8a1645010c0a2ee29d0d5340ba5a9f740829bc5e
danielalcover1 <1562889@uab.cat>
2023-05-08 13:39:55 +0200

    afegit clubs repo a providers

--- TFG_Frontend/src/app/di/providers.ts
+++ TFG_Frontend/src/app/di/providers.ts
@@ -4,9 +4,12 @@ import {GroupRepository} from "../core/league/group/group.repository";
 import {GroupDataRepository} from "../core/league/group/group.data-respository";
 import {TeamRepository} from "../core/club/team/team.repository";
 import {TeamDataRepository} from "../core/club/team/team.data-respository";
+import {ClubRepository} from "../core/club/club.repository";
+import {ClubDataRepository} from "../core/club/club.data-respository";
 
 export const AppProviders = [
   {provide: LeagueRepository, useClass: LeagueDataRepository},
   {provide: GroupRepository, useClass: GroupDataRepository},
-  {provide: TeamRepository, useClass: TeamDataRepository}
+  {provide: TeamRepository, useClass: TeamDataRepository},
+  {provide: ClubRepository, useClass: ClubDataRepository},
 ];
