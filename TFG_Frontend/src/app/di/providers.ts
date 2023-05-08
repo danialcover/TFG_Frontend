7e75d166b8c50ea0b89b232cdf7603750e907c17
danielalcover1 <1562889@uab.cat>
2023-05-08 13:02:57 +0200

    Afegit repo de group i teams a providers

--- TFG_Frontend/src/app/di/providers.ts
+++ TFG_Frontend/src/app/di/providers.ts
@@ -2,8 +2,11 @@ import {LeagueRepository} from "../core/league/league.repository";
 import {LeagueDataRepository} from "../core/league/league.data-respository";
 import {GroupRepository} from "../core/league/group/group.repository";
 import {GroupDataRepository} from "../core/league/group/group.data-respository";
+import {TeamRepository} from "../core/club/team/team.repository";
+import {TeamDataRepository} from "../core/club/team/team.data-respository";
 
 export const AppProviders = [
   {provide: LeagueRepository, useClass: LeagueDataRepository},
-  {provide: GroupRepository, useClass: GroupDataRepository}
+  {provide: GroupRepository, useClass: GroupDataRepository},
+  {provide: TeamRepository, useClass: TeamDataRepository}
 ];
