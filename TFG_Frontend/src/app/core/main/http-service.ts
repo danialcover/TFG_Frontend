533b1e898b2ba99a941d887351129818834a7bf0
danielalcover1 <1562889@uab.cat>
2023-05-11 13:16:14 +0200

    modificat el http service per tenir peticions sense autorització que son per fer el login

--- TFG_Frontend/src/app/core/main/http-service.ts
+++ TFG_Frontend/src/app/core/main/http-service.ts
@@ -1,6 +1,7 @@
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Injectable} from "@angular/core";
+import {Credentials} from "../profile/credentials";
 
 @Injectable({
   providedIn: "root"
@@ -16,12 +17,21 @@ export class HttpService {
     });
   }
 
+  private getNotLoginHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+  }
+
   public get<T>(url: string): Observable<T> {
     return this.http.get<T>(url, {headers: this.getHeaders()});
   }
 
-  public post<T>(url: string, data: any): Observable<T> {
+  public post<T>(url: string, data?: any): Observable<T> {
     return this.http.post<T>(url, data, {headers: this.getHeaders()});
+  }
+  public notLoginPost<T>(url: string, data?: Credentials): Observable<T> {
+    return this.http.post<T>(url, data, {headers: this.getNotLoginHeaders()});
   }
 
   public put<T>(url: string, data: any): Observable<T> {
