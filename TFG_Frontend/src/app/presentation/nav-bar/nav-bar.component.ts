71885dfa0ef1ae013dff8bbc34d1b06ce5a29e37
danielalcover1 <1562889@uab.cat>
2023-05-11 13:15:15 +0200

    afegides opcions de login i logout a la barra de navegació

--- TFG_Frontend/src/app/presentation/nav-bar/nav-bar.component.ts
+++ TFG_Frontend/src/app/presentation/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
+import {Profile} from "../../core/profile/profile";
+import {LocalStorageService} from "../../core/main/local-storage-service";
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,4 +10,34 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
 
+  protected ADMIN_ROLE = 1;
+  protected MANAGER_ROLE = 2;
+  protected REFEREE_ROLE = 3;
+
+  constructor(private router: Router,
+              private localStorageService: LocalStorageService) {
+  }
+
+  logout() {
+    this.localStorageService.removeItem('token');
+    this.localStorageService.removeItem('profile');
+    this.router.navigate(['/']);
+  }
+
+  getProfile() {
+    let profile = this.localStorageService.getItem('profile');
+    if (profile) {
+      let parsedProfile = JSON.parse(profile);
+      return new Profile(parsedProfile.id, parsedProfile.roles, parsedProfile.user.email, parsedProfile.user.first_name, parsedProfile.user.last_name);
+    }
+    return null;
+  }
+
+  getRole(role: number) {
+    let profile = this.getProfile();
+    if (profile) {
+      return profile.roles?.includes(role);
+    }
+    return false;
+  }
 }
