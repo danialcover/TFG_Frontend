542d78f5477ab9fe1fda26104940b371b0c4b1b5
danielalcover1 <1562889@uab.cat>
2023-05-11 14:30:21 +0200

    Creada pantalla de classificació d'una lliga i grup

---
+++ TFG_Frontend/src/app/presentation/team-groups-ranking/team-groups-ranking.component.ts
@@ -0,0 +1,40 @@
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {GroupTeamRepository} from "../../core/league/group-team/group-team.repository";
+import {GroupTeam} from "../../core/league/group-team/group-team";
+import {MatTableDataSource} from "@angular/material/table";
+import {Observable, Subscription} from "rxjs";
+import {Group} from "../../core/league/group/group";
+
+@Component({
+  selector: 'app-team-groups-ranking',
+  templateUrl: './team-groups-ranking.component.html',
+  styleUrls: ['./team-groups-ranking.component.scss']
+})
+export class TeamGroupsRankingComponent implements OnInit, OnChanges {
+
+  @Input() group: Observable<Group | null> = new Observable<Group | null>();
+  groupSubscription?: Subscription;
+  groupId?: number;
+
+  groupTeamsList: MatTableDataSource<GroupTeam> = new MatTableDataSource<GroupTeam>();
+  displayedColumns: string[] = ['id', 'name', 'points', 'gamesPlayed', 'gamesWon',
+    'gamesDraw', 'gamesLost', 'goalsScored', 'goalsConceded', 'lastFiveMatches'];
+
+  constructor(private groupTeamRepo: GroupTeamRepository) {
+  }
+
+  ngOnInit(): void {
+    this.groupSubscription = this.group.subscribe(group => {
+      this.groupId = group!.id;
+      this.groupTeamRepo.getList(this.groupId!).subscribe((groupTeams: GroupTeam[]) => {
+        this.groupTeamsList = new MatTableDataSource<GroupTeam>(groupTeams);
+      });
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+  }
+
+
+}
