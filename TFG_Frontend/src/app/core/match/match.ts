8c86efa54c3df181554183d01f1eac30e159069f
danielalcover1 <1562889@uab.cat>
2023-05-22 18:43:14 +0200

    Modificat al backend relacions entre match, groupTeam i team. Canvis necessaris al front per seguir l'exemple del back.

--- TFG_Frontend/src/app/core/match/match.ts
+++ TFG_Frontend/src/app/core/match/match.ts
@@ -1,22 +1,24 @@
 export class Match {
     id: number;
     group: number;
+    matchDay: number;
     referee?: number;
     location: number;
-    team1: number;
-    team2: number;
+    groupTeam1: number;
+    groupTeam2: number;
     comments?: string;
     date: Date;
     team1Result?: number;
     team2Result?: number;
 
-    constructor(id: number, group: number, location: number, team1: number, team2: number, date: Date,
+    constructor(id: number, group: number, matchDay: number, location: number, team1: number, team2: number, date: Date,
                 referee?: number, comments?: string, team1Result?: number, team2Result?: number) {
         this.id = id;
         this.group = group;
+        this.matchDay = matchDay;
         this.location = location;
-        this.team1 = team1;
-        this.team2 = team2;
+        this.groupTeam1 = team1;
+        this.groupTeam2 = team2;
         this.comments = comments;
         this.date = date;
         this.team1Result = team1Result;
