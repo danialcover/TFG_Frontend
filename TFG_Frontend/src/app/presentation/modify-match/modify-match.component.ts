22bdfe50b4ad7a8e923c6bd78e1fec26cf15c1b3
danielalcover1 <1562889@uab.cat>
2023-05-22 18:41:43 +0200

    Creada pantalla de modificar un partit per part d'un àrbitre

---
+++ TFG_Frontend/src/app/presentation/modify-match/modify-match.component.ts
@@ -0,0 +1,107 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {MatchRepository} from "../../core/match/match.repository";
+import {Match} from "../../core/match/match";
+import {TeamRepository} from "../../core/club/team/team.repository";
+import {Team} from "../../core/club/team/team";
+import {Location} from "@angular/common";
+import {GroupTeamRepository} from "../../core/league/group-team/group-team.repository";
+import {GroupTeam} from "../../core/league/group-team/group-team";
+
+class MatchTeams {
+  match: Match;
+  team1: Team;
+  team2: Team;
+  groupTeam1: GroupTeam;
+  groupTeam2: GroupTeam;
+
+  constructor(match: Match, team1: Team, team2: Team, groupTeam1: GroupTeam, groupTeam2: GroupTeam) {
+    this.match = match;
+    this.team1 = team1;
+    this.team2 = team2;
+    this.groupTeam1 = groupTeam1;
+    this.groupTeam2 = groupTeam2;
+  }
+}
+
+@Component({
+  selector: 'app-modify-match',
+  templateUrl: './modify-match.component.html',
+  styleUrls: ['./modify-match.component.scss']
+})
+export class ModifyMatchComponent implements OnInit {
+
+  matchId?: number;
+  team1Result?: number;
+  team2Result?: number;
+  match?: Match;
+  teamsList?: Team[];
+  groupTeamsList?: GroupTeam[];
+  teamMatch?: MatchTeams;
+
+  groupTeam1?: GroupTeam;
+  groupTeam2?: GroupTeam;
+
+  constructor(private route: ActivatedRoute,
+              private matchRepo: MatchRepository,
+              private teamRepo: TeamRepository,
+              private groupTeamRepo: GroupTeamRepository,
+              private router: Router,
+              private location: Location) {
+  }
+
+  ngOnInit() {
+    this.matchId = Number(this.route.snapshot.paramMap.get('id'));
+    this.matchRepo.get(this.matchId).subscribe(match => {
+      this.match = match;
+      this.groupTeamRepo.getList().subscribe(groupTeams => {
+        this.groupTeamsList = groupTeams;
+        this.teamRepo.getList().subscribe(teams => {
+          this.teamsList = teams;
+          this.formShowData();
+        });
+      });
+    });
+  }
+
+  formShowData() {
+    this.groupTeam1 = this.groupTeamsList?.find(groupTeam => groupTeam.id == this.match!.groupTeam1);
+    this.groupTeam2 = this.groupTeamsList?.find(groupTeam => groupTeam.id == this.match!.groupTeam2);
+    let team1Found = this.teamsList?.find(team => team.id == this.groupTeam1!.team);
+    let team2Found = this.teamsList?.find(team => team.id == this.groupTeam2!.team);
+    if (team1Found && team2Found && this.groupTeam1 && this.groupTeam2) {
+      this.teamMatch = new MatchTeams(this.match!, team1Found, team2Found, this.groupTeam1, this.groupTeam2);
+    }
+  }
+
+  submitForm() {
+    if (this.match && this.team1Result && this.team2Result) {
+      let matchResult = this.checkMatchResult();
+      let matchModified = new Match(this.match.id, this.match.group, this.match.matchDay, this.match.location, this.match.groupTeam1, this.match.groupTeam2, this.match.date);
+      matchModified.team1Result = this.team1Result;
+      matchModified.team2Result = this.team2Result;
+      this.matchRepo.modify(matchModified).subscribe(match => {
+        this.groupTeamRepo.modify(this.groupTeam1!, this.groupTeam2!, matchResult, this.team1Result!, this.team2Result!).subscribe();
+        this.location.back();
+      });
+    }
+    else {
+      //show message saying valid data needs to be introduced
+    }
+  }
+
+  checkMatchResult(): number {
+    if (this.team1Result && this.team2Result) {
+      if (this.team1Result > this.team2Result) {
+        return 0;
+      } else {
+        if (this.team1Result == this.team2Result) {
+          return 1;
+        } else {
+          return 2;
+        }
+      }
+    }
+    return 0;
+  }
+}
