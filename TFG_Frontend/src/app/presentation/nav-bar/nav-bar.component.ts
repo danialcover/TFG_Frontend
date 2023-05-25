578ad3a0f73421bbb5d224670296532147a972a3
danielalcover1 <1562889@uab.cat>
2023-05-25 11:41:35 +0200

    mogut funcions a utils.

--- TFG_Frontend/src/app/presentation/nav-bar/nav-bar.component.ts
+++ TFG_Frontend/src/app/presentation/nav-bar/nav-bar.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {Router} from "@angular/router";
 import {Profile} from "../../core/profile/profile";
 import {LocalStorageService} from "../../core/main/local-storage-service";
+import {Utils} from "../../core/main/utils";
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,38 +11,15 @@ import {LocalStorageService} from "../../core/main/local-storage-service";
 })
 export class NavBarComponent {
 
-  protected ADMIN_ROLE = 1;
-  protected MANAGER_ROLE = 2;
-  protected REFEREE_ROLE = 3;
-
-  profile? : Profile;
-
   constructor(private router: Router,
               private localStorageService: LocalStorageService) {
   }
 
   logout() {
-    delete this.profile;
     this.localStorageService.removeItem('token');
     this.localStorageService.removeItem('profile');
     this.router.navigate(['/']);
   }
 
-  getProfile() {
-    let profile = this.localStorageService.getItem('profile');
-    if (profile) {
-      let parsedProfile = JSON.parse(profile);
-      this.profile = new Profile(parsedProfile.id, parsedProfile.roles, parsedProfile.email, parsedProfile.firstName, parsedProfile.lastName);
-      return this.profile;
-    }
-    return null;
-  }
-
-  getRole(role: number) {
-    let profile = this.getProfile();
-    if (profile) {
-      return profile.roles?.includes(role);
-    }
-    return false;
-  }
+  protected readonly Utils = Utils;
 }
